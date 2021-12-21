6be13a0cc13671e0c868b9691339e923134a51f0
marcodeiulio <mdeiulio@icloud.com>
2021-12-21 16:09:20 +0100

    - Added stamp on HTML

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
 
-const userAge = parseInt(prompt('Quanti anni hai?', '69').trim());
+const discountMessage = document.getElementById('discount')
+
+const userAge = parseInt(prompt('Quanti anni hai?', '70').trim());
 console.log('userAge: ', userAge);
 
 const userDistance = parseInt(prompt('Per quanti chilometri vuoi viaggiare?', '400').trim());
@@ -21,10 +23,15 @@ console.log('ticketPrice: ', ticketPrice);
 
 if (userAge < 18) {
 	ticketPrice = ticketPrice * 0.80;
+	discountMessage.innerText = 'Data la Sua età, lo sconto applicato è del 20%.'
 	// 15 --> 67.2
 } else if (userAge > 65) {
 	ticketPrice = ticketPrice * 0.60;
+	discountMessage.innerText = 'Data la Sua età, lo sconto applicato è del 40%.'
 	// 70 --> 50.4
+} else {
+	discountMessage.innerText = 'Non ci sono sconti applicabili.'
 }
 
+document.getElementById('ticketPrice').innerText = ticketPrice.toFixed(2) + '€';
 console.log('Discounted ticketPrice: ', ticketPrice.toFixed(2), '€');
